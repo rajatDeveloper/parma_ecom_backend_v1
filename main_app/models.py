53253e049a1e6d28a0191d2763aa7d3a1224b5ff
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2024-02-08 13:32:40 +0530

    Update models.py

--- main_app/models.py
+++ main_app/models.py
@@ -6,9 +6,6 @@ class StatusType(models.TextChoices):
     PENDING = "pending", "Pending"
     DISPATCH = "dispatch", "Dispatch"
     DILVERIED = "dilveried", "Dilveried"
-    
-    
-      
 
 
 class Product(models.Model):
@@ -20,9 +17,7 @@ class Product(models.Model):
     afterDiscountPrice = models.IntegerField(default=0)
     category = models.CharField(max_length=50, default="")
     isSale = models.BooleanField(default=False)
-    stock = models.IntegerField(default=0 , null=True , blank=True)
-    
-    
+    stock = models.IntegerField(default=0, null=True, blank=True)
 
     class Meta:
         verbose_name = "Product"
@@ -30,48 +25,57 @@ class Product(models.Model):
 
     def __str__(self):
         return self.productName
-    
-    
-class CartProduct(models.Model):
-        
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product" , null=True)
-    quantity = models.IntegerField(default=0 , null=True , blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_cart_product")
-    
-    def __str__(self):
-        return self.user.username +" " +self.product.productName + " " + str(self.quantity) 
 
-        
-        
-class Address (models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_addresses")
+
+class CartProduct(models.Model):
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product", null=True
+    )
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_cart_product"
+    )
+    isOrderItem = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return (
+            self.user.username
+            + " "
+            + self.product.productName
+            + " "
+            + str(self.quantity)
+        )
+
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_addresses"
+    )
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     zipCode = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    
+
     def __str__(self):
-        return self.address +" " +self.user.username  
-    
-    
-    
-class Order (models.Model):
+        return self.address + " " + self.user.username
+
+
+class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_orders")
-    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="orders")
+    address = models.ForeignKey(
+        Address, on_delete=models.CASCADE, related_name="orders"
+    )
     cart_products = models.ManyToManyField(CartProduct)
     total = models.IntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
-    payment_id = models.CharField(max_length=50, default="" , null=True , blank=True)
+    payment_id = models.CharField(max_length=50, default="", null=True, blank=True)
     dilveryStatus = models.CharField(
         max_length=10,
         choices=StatusType.choices,  # Use the .choices property of the TextChoices class
     )
-    
+
     def __str__(self):
-        return self.user.username+" "+str(self.date)      
-    
-    
-    
-    
+        return self.user.username + " " + str(self.date)
